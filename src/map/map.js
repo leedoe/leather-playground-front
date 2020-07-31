0597643f1e4f36cd655c89181693acfba7141c4f
DOJUN <leedoe0709@gmail.com>
2020-07-31 19:10:26 +0900

    가게마다 마커 색 다르게 나올 수 있게 수정 보이는 화면의 마커만 출력

--- src/map/map.js
+++ src/map/map.js
@@ -5,6 +5,10 @@ import { withStyles } from '@material-ui/core'
 import { withSnackbar } from 'notistack'
 import { withRouter } from 'react-router-dom'
 
+import leatherStoreMarkerIcon from '../img/marker-leatherstore.png'
+import materialStoreMarkerIcon from '../img/marker-materialstore.png'
+import commonStoreMarkerIcon from '../img/marker-commonStore.png'
+
 const useStyles = theme => ({
   divider: {
     marginTop: theme.spacing(3),
@@ -21,12 +25,21 @@ const useStyles = theme => ({
 
 class NMap extends React.Component {
   state = {
-    stores: [],
+    workshops: [],
+    toolstores: [],
+    leatherstores: [],
+    commonstores: [],
+    materialstores: [],
     bounds: '',
     southWest: '',
     northEast: '',
     lngSpan: '',
     latSpan: '',
+    isShowWorkShops: true,
+    isShowToolStores: true,
+    isShowLeatherStores: true,
+    isShowCommonStores: true,
+    isShowMaterialStores: true,
   }
 
   componentDidMount() {
@@ -43,39 +56,162 @@ class NMap extends React.Component {
       latSpan,
     })
 
+    new window.naver.maps.Event.addListener(this.mapRef.instance, 'zoom_changed', () => {
+      if(this.state.isShowWorkShops) {
+        this.updateMarkers(this.state.workshops);
+      }
+      
+      if(this.state.isShowToolStores) {
+        this.updateMarkers(this.state.toolstores);
+      }
+      
+      if(this.state.isShowLeatherStores) {
+        this.updateMarkers(this.state.leatherstores);
+      }
+      
+      if(this.state.isShowCommonStores) {
+        this.updateMarkers(this.state.commonstores);
+      }
+
+      if(this.state.isShowMaterialStores) {
+        this.updateMarkers(this.state.materialstores)
+      }
+    });
+  
+    new window.naver.maps.Event.addListener(this.mapRef.instance, 'dragend', () => {
+      if(this.state.isShowWorkShops) {
+        this.updateMarkers(this.state.workshops);
+      }
+      
+      if(this.state.isShowToolStores) {
+        this.updateMarkers(this.state.toolstores);
+      }
+      
+      if(this.state.isShowLeatherStores) {
+        this.updateMarkers(this.state.leatherstores);
+      }
+      
+      if(this.state.isShowCommonStores) {
+        this.updateMarkers(this.state.commonstores);
+      }
+
+      if(this.state.isShowMaterialStores) {
+        this.updateMarkers(this.state.materialstores)
+      }
+    });
+
     Axios.get(`${process.env.REACT_APP_SERVERURL}/api/stores/`).then((response) => {
       const markers = []
       for(const store of response.data) {
-        const marker = new window.naver.maps.Marker({
-          map: this.mapRef.instance,
-          position: {lat: store.x, lng: store.y},
-          title: store.name,
+        if(store.category === 0) {
+          const marker = new window.naver.maps.Marker({
+            map: this.mapRef.instance,
+            position: {lat: store.x, lng: store.y},
+            title: store.name,
+          })
+  
+          window.naver.maps.Event.addListener(marker, 'click', () => {
+            this.props.changeCurrentStore(store)
+          })
           
-        })
+          this.state.workshops.push(marker)
+        } else if (store.category === 1) { // toolstore
 
-        window.naver.maps.Event.addListener(marker, 'click', () => {
-          this.props.changeCurrentStore(store)
-        })
+        } else if (store.category === 2) { // leatherstore
+          const marker = new window.naver.maps.Marker({
+            map: this.mapRef.instance,
+            position: {lat: store.x, lng: store.y},
+            title: store.name,
+            icon: leatherStoreMarkerIcon
+          })
+  
+          window.naver.maps.Event.addListener(marker, 'click', () => {
+            this.props.changeCurrentStore(store)
+          })
+          
+          this.state.leatherstores.push(marker)
+        } else if(store.category === 3) { // commonstore
+          const marker = new window.naver.maps.Marker({
+            map: this.mapRef.instance,
+            position: {lat: store.x, lng: store.y},
+            title: store.name,
+            icon: commonStoreMarkerIcon
+          })
+  
+          window.naver.maps.Event.addListener(marker, 'click', () => {
+            this.props.changeCurrentStore(store)
+          })
+          
+          this.state.commonstores.push(marker)
+        } else if (store.category === 4) { // materialstore
+          const marker = new window.naver.maps.Marker({
+            map: this.mapRef.instance,
+            position: {lat: store.x, lng: store.y},
+            title: store.name,
+            icon: materialStoreMarkerIcon
+          })
+  
+          window.naver.maps.Event.addListener(marker, 'click', () => {
+            this.props.changeCurrentStore(store)
+          })
+          
+          this.state.materialstores.push(marker)
+        }
         
-        markers.push(marker)
       }
       
-      this.setState({stores: markers, nowLoading: false,})
+      this.setState({nowLoading: false,})
     })
   }
 
-  updateMarkers = () => {
+  showOnlyWorkshop = () => {
     const mapBounds = this.mapRef.getBounds()
-    for(const marker of this.state.stores) {
+    for(const workshop of this.state.workshops) {
+      if(workshop.getMap()) {
+        continue
+      } else {
+        workshop.setMap(this.mapRef.instance)
+      }
+    }
+
+    for(const toolstore of this.state.toolstores) {
+
+    }
+  }
+
+  showMarkers = (markers) => {
+    const mapBounds = this.mapRef.getBounds()
+    for(const marker of markers) {
+      if(marker.getMap()) {
+        continue
+      } else {
+        marker.setMap(this.mapRef.instance)
+      }
+    }
+  }
+
+  hideMarkers = (markers) => {
+    for(const marker of markers) {
+      if(marker.getMap()) {
+        marker.setMap(null)
+      } 
+    }
+  }
+
+  
+
+  updateMarkers = (markers) => {
+    const mapBounds = this.mapRef.getBounds()
+    for(const marker of markers) {
       if (mapBounds.hasLatLng(marker.position)) {
         if(marker.getMap()) {
-          return
+          continue
         } else {
           marker.setMap(this.mapRef.instance)
         }
       } else {
         if(!marker.getMap()) {
-          return
+          continue
         } else {
           marker.setMap(null)
         }
